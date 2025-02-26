1ad4433c9db6cc6288204f1d93f303facca86675
choiquychoi <congquynguyen1349@gmail.com>
2025-02-26 15:52:32 +0700

    little update for eslint

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -28,39 +28,39 @@ function AppBar() {
             gap: 2,
             overflowX: 'auto'
         }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2}}>
-            <AppsIcon sx={{ color: 'primary.main'}} />
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5}}>
-                <SvgIcon component={trelloLogo} inheritViewBox sx={{ color: 'primary.main'}} />
-                <Typography variant="span" sx={{ fontSize: "1.5rem", fontWeight: 'Bold' ,color: 'primary.main', ml: 0.5}}>Trello</Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                <AppsIcon sx={{ color: 'primary.main' }} />
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+                    <SvgIcon component={trelloLogo} inheritViewBox sx={{ color: 'primary.main' }} />
+                    <Typography variant="span" sx={{ fontSize: '1.5rem', fontWeight: 'Bold', color: 'primary.main', ml: 0.5 }}>Trello</Typography>
+                </Box>
+
+                <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 1 }}>
+                    <WorkSpaces />
+                    <Recent />
+                    <Starred />
+                    <Templates />
+                    <Button variant='outlined' startIcon={< LibraryAddIcon/>}>Invite</Button>
+                </Box>
             </Box>
 
-            <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 1 }}>
-                <WorkSpaces />
-                <Recent />
-                <Starred />
-                <Templates />
-                <Button variant='outlined' startIcon={< LibraryAddIcon/>}>Invite</Button>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                <TextField id="outlined-search" label="Search..." type="search" size='small' sx={{ minWidth: '120px' }}/>
+
+                <ModeSelect />
+
+                <Tooltip title="Notification">
+                    <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }} >
+                        <NotificationsIcon sx={{ color: 'primary.main' }}/>
+                    </Badge>
+                </Tooltip>
+
+                <Tooltip title="Help">
+                    <HelpIcon sx={{ color: 'primary.main', cursor: 'pointer' }}/>
+                </Tooltip>
+
+                <Profiles />
             </Box>
-        </Box>
-
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2}}>
-            <TextField id="outlined-search" label="Search..." type="search" size='small' sx={{ minWidth: '120px' }}/>
-
-            <ModeSelect />
-
-            <Tooltip title="Notification">
-                <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer'}} >
-                    <NotificationsIcon sx={{ color: 'primary.main'}}/>
-                </Badge>
-            </Tooltip>
-
-            <Tooltip title="Help">
-                <HelpIcon  sx={{  color: 'primary.main', cursor: 'pointer'}}/>
-            </Tooltip>
-
-            <Profiles />
-        </Box>
         </Box>
     )
 }
