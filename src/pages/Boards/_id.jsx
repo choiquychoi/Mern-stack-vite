f0c05c0c4dc79bc6e95c2af21f49f989ae673392
choiquychoi <congquynguyen1349@gmail.com>
2025-04-16 15:20:28 +0700

    update UI UX

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,16 +2,17 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from '~/pages/Boards/BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+
+// import { mockData } from '~/apis/mock-data'
 import { useEffect, useState } from 'react'
 import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
-    const [board, setboard ] = useState(null)
+    const [board, setboard] = useState(null)
 
     useEffect(() => {
         // tạm thời fix cứng boardId, flow chuẩn chính về sau sẻ sử dụng react-router-dom để lấy chuẩn boardId từ URL về
-        const boardId = '67f9e0830f716a853e563727'
+        const boardId = '67fe651251fb95b10e9c7255'
         // call API
         fetchBoardDetailsAPI(boardId).then((board) => {
             setboard(board)
