6f6622e4b383bb4a0bce792a0b36e6fe2ac02112
choiquychoi <congquynguyen1349@gmail.com>
2025-04-17 17:42:18 +0700

    create API for card and column

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,7 +3,7 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from '~/pages/Boards/BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
 import { useEffect, useState } from 'react'
 import { fetchBoardDetailsAPI } from '~/apis'
 
@@ -24,9 +24,9 @@ function Board() {
 
             <AppBar />
 
-            <BoardBar board={ mockData.board} />
+            <BoardBar board={ board} />
 
-            <BoardContent board={ mockData.board}/>
+            <BoardContent board={ board}/>
 
         </Container>
     )
