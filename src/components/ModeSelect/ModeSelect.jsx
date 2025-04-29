fe7a514ae348a2ddc43dc09d6ba4d158c3baa91e
choiquychoi <congquynguyen1349@gmail.com>
2025-04-29 09:51:19 +0700

    fix right the DarkModeOutlined

--- src/components/ModeSelect/ModeSelect.jsx
+++ src/components/ModeSelect/ModeSelect.jsx
@@ -5,7 +5,7 @@ import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
 import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeOutLinedIcon from '@mui/icons-material/DarkModeOutLined'
+import DarkModeOutLinedIcon from '@mui/icons-material/DarkModeOutlined'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 
 function ModeSelect() {
