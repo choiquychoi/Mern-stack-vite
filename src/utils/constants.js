38ca80ade0528713f0a333cc45fc709922401861
choiquychoi <congquynguyen1349@gmail.com>
2025-05-21 18:55:20 +0700

    stop run FE on local

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 let apiRoot = ''
-if (import.meta.env.VITE_BUILD_MODE === 'dev') {
-    apiRoot = 'http://localhost:8017'
-}
+// if (import.meta.env.VITE_BUILD_MODE === 'dev') {
+//     apiRoot = 'http://localhost:8017'
+// }
 if (import.meta.env.VITE_BUILD_MODE === 'production') {
     apiRoot = 'https://trello-api-jhwf.onrender.com'
 }
